0dd757813545519d494f111030156f2d2ae0783a
totodore <prevottheodore@gmail.com>
2020-11-22 19:43:21 +0100

    create database if not exists

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -1,6 +1,7 @@
 import { User } from './../models/user.model';
 import { Sequelize } from 'sequelize-typescript';
 import { Logger } from './../utils/logger';
+import * as mysql from "mysql2/promise"
 export class DBService {
 
   private readonly _logger = new Logger(this);
@@ -8,9 +9,8 @@ export class DBService {
 	private readonly _sequelize = new Sequelize({
 		database: process.env.DB_NAME,
 		host: process.env.DB_HOST,
-		port: parseInt(process.env.DB_PORT),
-		username: process.env.DB_USER,
-		password: process.env.DB_PASS,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USER,
 		dialect: "mysql",
 		logging: false,
 		models: [
@@ -20,10 +20,20 @@ export class DBService {
   
   /**
    * Initilize the Database connexion
+   * Create a databse if it not exists
    * Throw errors that should be catch when calling this method
    */
   public async init(removeOld = false) {
-			await this._sequelize.sync({ force: removeOld });
+    
+    (await mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      user: process.env.DB_USER
+    })).query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`);
+
+    await this._sequelize.sync({ force: removeOld });
+
+    this._logger.log("Connected to Database");
   }
 
 }
