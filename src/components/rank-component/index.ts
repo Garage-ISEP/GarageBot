20b23fa1a671128e2d6e9d7513b7c3d3057f28c6
totodore <prevottheodore@gmail.com>
2020-11-22 19:04:09 +0100

    feature > extending default component

--- src/components/rank-component/index.ts
+++ src/components/rank-component/index.ts
@@ -4,12 +4,7 @@ import { DBService } from './../../services/db.service';
 /**
  * Faites votre life ici !
  */
-export class RankComponent implements DefaultComponent {
-  
-  constructor(
-    private _dbService: DBService,
-    private _botService: BotService
-  ) { }
+export class RankComponent extends DefaultComponent {
   
   public async init() {
     //Init le systeme de component
--- src/components/voice-channel-component/index.ts
+++ src/components/voice-channel-component/index.ts
@@ -2,13 +2,8 @@ import { BotService } from './../../services/bot.service';
 import { DBService } from '../../services/db.service';
 import { DefaultComponent } from './../component.default';
 
-export class VoiceChannelComponent implements DefaultComponent {
+export class VoiceChannelComponent extends DefaultComponent {
 
-  constructor(
-    private _dbService: DBService,
-    private _botService: BotService
-  ) { }
-  
   public async init() {
 
   }
