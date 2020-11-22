defedea27188de6ec597fae8e6f31a85dbe17dd6
totodore <prevottheodore@gmail.com>
2020-11-22 19:03:44 +0100

    feature > abstract class for default component

--- src/components/component.default.ts
+++ src/components/component.default.ts
@@ -1,3 +1,10 @@
-export interface DefaultComponent {
-  init(): void | Promise<void>;
+import { BotService } from './../services/bot.service';
+import { DBService } from './../services/db.service';
+export abstract class DefaultComponent {
+  constructor(
+    protected _dbService: DBService,
+    protected _botService: BotService
+  ) { }
+
+  public abstract init(): Promise<void>;
 }
