0f8a0b118de02b93ebb2bc84a63b1967367554b9
totodore <prevottheodore@gmail.com>
2020-11-22 18:19:23 +0100

    bot service and db service

---
+++ src/services/bot.service.ts
@@ -0,0 +1,69 @@
+import { DBService } from './db.service';
+import { Logger } from './../utils/logger';
+import * as Discord from "discord.js";
+import { generate as uuid } from "short-uuid";
+
+export class BotService {
+
+  private _bot: Discord.Client = new Discord.Client();
+  private _logger: Logger = new Logger(this);
+
+  private _listeners: ListenerData<keyof Discord.ClientEvents> = {};
+  
+  /**
+   * Initilize the bot connection
+   * Throw errors that should be catch when calling this method
+   */
+  public async init() {
+    await this._bot.login(process.env.BOT_TOKEN);
+  }
+
+  /**
+   * Add en event listener to discord
+   * @returns an ID that you can use to remove the listener 
+   */
+  public addEventListener<K extends keyof Discord.ClientEvents>(event: K, listener: (...args: Discord.ClientEvents[K]) => void): string {
+    this._bot.on(event, listener);
+    const id = uuid();
+    this._listeners[id] = {
+      listener,
+      name: event
+    }
+    return id;
+  }
+
+  /**
+   * Remove an event listener added precedently
+   * throw an error if the id doesn't exists
+   * @param id the id of the event
+   */
+  public removeEventListener(id: string): void {
+    const event = this._listeners[id];
+    if (!event)
+      throw "Bad ID for event listener";
+    this._bot.off(event.name, event.listener);
+  }
+
+  /**
+   * Get a guild from its id
+   */
+  public getGuild(guildId: string): Discord.Guild {
+    return this._bot.guilds.resolve(guildId);
+  }
+
+  /**
+   * Get all the guilds that the server has
+   */
+  public getAllGuilds(): Discord.Guild[] {
+    return this._bot.guilds.cache.array();
+  }
+}
+
+
+
+interface ListenerData<K extends keyof Discord.ClientEvents> {
+  [key: string]: {
+    name: K,
+    listener: (...args: Discord.ClientEvents[K]) => void
+  }
+}
