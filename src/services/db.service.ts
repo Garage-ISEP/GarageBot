4e511f99a2eb33096293e72b4b24f48e5be4229f
totodore <prevottheodore@gmail.com>
2020-11-22 19:04:20 +0100

    feature > adding db connexion

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -1,3 +1,29 @@
+import { User } from './../models/user.model';
+import { Sequelize } from 'sequelize-typescript';
+import { Logger } from './../utils/logger';
 export class DBService {
+
+  private readonly _logger = new Logger(this);
+
+	private readonly _sequelize = new Sequelize({
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
+		port: parseInt(process.env.DB_PORT),
+		username: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		dialect: "mysql",
+		logging: false,
+		models: [
+      User
+    ],
+  });
   
+  /**
+   * Initilize the Database connexion
+   * Throw errors that should be catch when calling this method
+   */
+  public async init(removeOld = false) {
+			await this._sequelize.sync({ force: removeOld });
+  }
+
 }
